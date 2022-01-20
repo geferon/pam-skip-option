485bca4ab5a4bd9b3bf6e5500412a303cb9f072d
Reispfannenfresser <34158734+Reispfannenfresser@users.noreply.github.com>
2022-01-20 01:17:59 +0100

    Initial Code

---
+++ pam_skip_option/lua/pam/server/extensions/skip_option.lua
@@ -0,0 +1,14 @@
+local name = "skip_option"
+PAM_EXTENSION.name = name
+PAM_EXTENSION.enabled = true
+
+local setting_namespace = PAM.setting_namespace:AddChild(name)
+local reset_percentage = setting_namespace:AddSetting("round_reset_percentage", pacoman.TYPE_PERCENTAGE, 1)
+
+function PAM_EXTENSION:RegisterSpecialOptions()
+	PAM.RegisterOption("skip", function()
+		PAM.Cancel()
+		local round_limit = PAM.setting_namespace:GetChild("custom_round_counter"):GetSetting("round_limit")
+		PAM.custom_round_counter = round_limit:GetActiveValue() * (1 - reset_percentage:GetActiveValue())
+	end)
+end
