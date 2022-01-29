1829c6068b3ff6c9627216f8228d6e5088105aba
Reispfannenfresser <34158734+Reispfannenfresser@users.noreply.github.com>
2022-01-29 02:16:50 +0100

    Compatibility fixes with round counter update

--- pam_skip_option/lua/pam/server/extensions/skip_option.lua
+++ pam_skip_option/lua/pam/server/extensions/skip_option.lua
@@ -4,13 +4,18 @@ PAM_EXTENSION.enabled = true
 
 local setting_namespace = PAM.setting_namespace:AddChild(name)
 local reset_percentage = setting_namespace:AddSetting("round_reset_percentage", pacoman.TYPE_PERCENTAGE, 1)
+local round_limit
+
+function PAM_EXTENSION:Initialize()
+	round_limit = PAM.setting_namespace:GetChild("custom_round_counter"):GetSetting("round_limit")
+end
 
 function PAM_EXTENSION:RegisterSpecialOptions()
 	if PAM.vote_type ~= "map" then return end
 
 	PAM.RegisterOption("keep_playing", function()
 		PAM.Cancel()
-		local round_limit = PAM.setting_namespace:GetChild("custom_round_counter"):GetSetting("round_limit")
-		PAM.custom_round_counter = round_limit:GetActiveValue() * (1 - reset_percentage:GetActiveValue())
+		local new_round_count = round_limit:GetActiveValue() * (1 - reset_percentage:GetActiveValue())
+		PAM.extension_handler.RunEvent("SetRoundCounter", new_round_count)
 	end)
 end
